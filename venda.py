16b5ecac84d313f4fb48636e0695eecec66aab03
calbuch99 <alexcalbuch99@gmail.com>
2025-07-03 21:50:39 -0300

    vendas

--- venda.py
+++ venda.py
@@ -10,7 +10,7 @@ def menu_vendas():
     print("|             3 - Sair                           |")
     print("|------------------------------------------------|")
 
-    conexao = conecta_db
+    conexao = conecta_db()
 
     while True:
         opcao = input("Escolha uma opção: ")
@@ -18,7 +18,7 @@ def menu_vendas():
         if opcao ==  "1":
             consultar_venda()
         elif opcao == "2":
-            inserir_venda()
+            inserir_venda(conexao)
 
         elif opcao == "3":
             break
@@ -28,8 +28,72 @@ def menu_vendas():
 def consultar_venda():
     print("Não implementado") 
 
-def inserir_venda():
+def insert_item_venda(conexao,item_venda) :
+    cursor = conexao.cursor()
+    sql_insert_item = """
+           insert into itens_vendas(id_venda, id_produto,qtde, valor_unitario, valor_total)
+           values (%s,%s,%s,%s,%s)
+           """
+    cursor.execute(sql_insert_item,item_venda)
+    conexao.commit()
+
+
+
+
+def inserir_venda(conexao):
+    venda = alimentar_venda(conexao)
+    itens_venda = alimentar_itens_venda()
+    total_venda = calcular_total_venda(itens_venda)
+
+    id_venda = inserir_venda(venda,total_venda)
+
+
+    for item in itens_venda:
+        item_data = (id_venda, item['id_produto'], item['quantidade'],
+                     item['preco_unitario'], item['valor_total'])
+
+        insert_item_venda(conexao,item_data)
+
+def alimentar_venda(conexao): #Dados 
     id_cliente = input("Digite o ID do cliente")
     data_venda = datetime.now()
-    numero = 0
+    numero = input("Digite o Número da venda")
     valor_venda = 0
+    
+    return (id_cliente, data_venda, numero, valor_venda)
+
+def insert_venda(conexao, id_cliente, data_venda, numero, valor_venda):#Responsável por salvar dados
+    cursor = conexao.cursor()
+    sql_insert_venda = """
+                       insert into venda(id_cliente, data_venda, numero_venda, valor_venda) 
+                       values (%s,%s,%s,%s) RETURNING id
+                       """
+    dados_venda = (id_cliente,data_venda,numero,valor_venda)
+    cursor.execute(sql_insert_venda,dados_venda)
+    venda_id = cursor.fetchone()[0]
+    return venda_id
+
+def alimentar_itens_venda(itens_venda):
+    itens_venda = []
+    while(True):
+        id_produto = int(input("Digite o ID do produto : "))
+        quantidade = float(input("Digite a quantidade :  "))
+        preco_unitario = float(input("Digite o preço unitário : "))
+        valor_total = quantidade * preco_unitario
+
+        itens_venda.append({"id_produto": id_produto,
+                            "quantidade": quantidade,
+                            "preco_unitario": preco_unitario,
+                            "valor_total": valor_total})
+        print(itens_venda)
+        continua = input("Deseja adicionar outro item?  (S/N): ")
+        
+        if continua == "N":
+            break
+    return itens_venda
+
+def calcular_total_venda(itens_venda):
+    total_venda = 0
+    for item in itens_venda:
+        total_venda = total_venda + item['valor_total']
+    return total_venda
